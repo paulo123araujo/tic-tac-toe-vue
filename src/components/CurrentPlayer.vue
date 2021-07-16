6168fd14d1367295d9c4be857bc935a80b617017
Paulo Araujo <paulofelipe_jau7654@hotmail.com>
2021-07-16 18:23:01 -0300

    decompose board into small components

---
+++ src/components/CurrentPlayer.vue
@@ -0,0 +1,27 @@
+<template>
+    <span>Vez do jogador: <strong>{{ currentPlayer.name }}</strong></span>
+</template>
+
+<script>
+    export default {
+        name: 'CurrentPlayer',
+        props: {
+            players: {
+                type: [],
+                default: [],
+            }
+        },
+        computed: {
+            currentPlayer() {
+                return this.players.filter(player => player.current)[0]
+            }
+        }
+    }
+</script>
+
+<style>
+    span {
+        text-align: center;
+        font-size: 32px;
+    }
+</style>
---
+++ src/components/NewGameButton.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="button">
+        <button type="button" @click="$emit('newGame')">Novo Jogo</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'NewGameButton',
+        emits: ['newGame']
+    }
+</script>
+
+<style>
+    .button {
+        width: 100%;
+        margin: 0 auto;
+    }
+
+    .button button[type="button"] {
+        border: 1px solid #434343;
+        padding: 15px;
+        background-color: #e3e3e3;
+        color:  #333333;
+        border-radius: 8px;
+        width: 300px;
+        margin: 50px;
+        cursor: pointer;
+        font-weight: bold;
+    }
+
+    .button button[type="button"]:hover {
+        background-color: #333333;
+        color: #e3e3e3;
+        transition:  .5s all;
+    }
+</style>
